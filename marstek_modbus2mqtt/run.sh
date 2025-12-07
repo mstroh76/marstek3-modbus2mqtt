4224968b9fbe0997904bc1bcd4dc1a100bc35d08
mstroh <m.stroh76@gmail.com>
2025-12-07 17:34:08 +0100

    Update startup message for Modbus2MQTT Add-on

--- marstek_modbus2mqtt/run.sh
+++ marstek_modbus2mqtt/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "[INFO] Starting Marstek Modbus2MQTT Add-on..."
+echo "[INFO] Starting Marstek 3.0 Modbus2MQTT Add-on..."
 
 MODBUS_HOST=$(jq -r '.modbus_host' /data/options.json)
 MODBUS_PORT=$(jq -r '.modbus_port' /data/options.json)
